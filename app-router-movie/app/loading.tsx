bcffa035506449cf69611a5708acfbd12b1f129d
Layton <jihwan8807@naver.com>
2024-07-09 11:42:09 +0900

    로딩 페이지 추가

---
+++ app-router-movie/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+const loading = () => {
+  return (
+    <>
+      <LoadingSpinner />
+    </>
+  );
+};
+
+export default loading;
